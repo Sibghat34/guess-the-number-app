6ffaa44cb0b8bca3cdb9fc2e24377b56884fb5fa
Sibghat Ullah <sibghat.aamir34@gmail.com>
2023-09-17 03:11:27 +0500

    main screen created with gradients

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StyleSheet} from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import { ImageBackground, StyleSheet } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <StartGameScreen/>
+    <LinearGradient colors={["#451952", "#F39F5A"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/images/background.webp")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
- 
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.1,
+  },
 });
